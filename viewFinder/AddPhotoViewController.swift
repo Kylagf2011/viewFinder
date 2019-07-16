765dcb95fc6dddcc34b048a3f43f73161fa25a92
Kylagf2011 <kylagf2011@gmail.com>
2019-07-16 11:50:09 -0700

    Complete Iteration 4. Minimum Viable Product.

--- viewFinder/AddPhotoViewController.swift
+++ viewFinder/AddPhotoViewController.swift
@@ -73,15 +73,4 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
             navigationController?.popViewController(animated: true)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
